673ea256811e131b506aaa81e2ba7cb2fd26dce0
barbertr0n <barberthomas9@gmail.com>
2024-10-24 01:56:15 +0100

    Added a navbar component and a line break

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,15 @@ import Header from './components/Header.jsx';
 import Navbar from './components/Navbar.jsx';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
 	<>
-	  <Header />
-	  <Navbar />
-	<p>A site to display all of my my accolades and projects.</p>
+		<Header />
+		<Navbar />
+		<hr/>
+		<hr/>
+		<p>A site to display all of my my accolades and projects.</p>
 	</>
-  )
-}
+	)
+};
 
-export default App
+export default App;
