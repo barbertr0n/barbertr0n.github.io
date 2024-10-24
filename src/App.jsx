7077a8167dfa7531c8e99f6f8ca9765cb023d9a4
barbertr0n <barberthomas9@gmail.com>
2024-10-24 01:00:33 +0100

    created first components and set up file structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
+import Header from './components/Header.jsx';
+import Navbar from './components/Navbar.jsx';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
 	<>
-	<h1>My Portfolio</h1>
+	  <Header />
+	  <Navbar />
 	<p>A site to display all of my my accolades and projects.</p>
 	</>
   )
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+const Navbar = () => {
+	return (
+		<>
+		<ul>
+			<li><a>Home</a></li>
+			<li><a>About</a></li>
+			<li><a>Projects</a></li>
+			<li><a>Contact</a></li>
+		</ul>
+		</>
+	)
+};
+export default Navbar;
