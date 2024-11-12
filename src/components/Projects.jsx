0bb1f5c55da6280d898dfea78c4110a4ab7396c6
barbertr0n <barberthomas9@gmail.com>
2024-11-12 22:36:59 +0000

    Added a fetch function to retieve data from a local JSOn file to update the state of the project information

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {useState} from 'react';
+import { useState } from 'react';
 import Project from './project.jsx';
 import Project1 from '../assets/Project1.png';
 import Project2 from '../assets/Project2.png';
@@ -8,51 +8,48 @@ import Project4 from '../assets/Project4.png';
 import Project5 from '../assets/Project5.png';
 
 const Projects = () => {
+	const Project1Info = 'Project 1 information';
+
 	const [currentProject, setCurrentProject] = useState('Project 1');
 	const [currentProjectImage, setCurrentProjectImage] = useState(Project1);
-	const [currentProjectText, setCurrentProjectText] = useState(selectProjectText());
+	const [currentProjectText, setCurrentProjectText] = useState(Project1Info);
 
-	function handleClick(name, img) {		
+	function handleClick(name, img) {
 		setCurrentProject(name);
 		setCurrentProjectImage(img);
-		setCurrentProjectText(selectProjectText(name));
+		getJSON(name);
 	}
-	
-	function selectProjectText(name) {
-		let info;
-		switch(name) {
-			default:
-				info = "Project 1 Information"
-				break;
-			case 'Project 2':
-				info = "Project 2 Information"
-				break;
-			case 'Project 3':
-				info = "Project 3 Information"
-				break;
-			case 'Project 4':
-				info = "Project 4 Information"
-				break;
-			case 'Project 5':
-				info = "Project 5 Information"
-				break;
-		};
-		return info
+
+	function getJSON(name) {
+		fetch('project-info.json')
+		.then((res) => res.json())
+		.then((data) => {
+			setCurrentProjectText(data[name])})
 	}
 
 	return (
-		<div id='projects-container'>
-			<aside id='project-title-container'>
-				<div className='project-title' onClick={() => handleClick('Project 1', Project1)} >Project 1</div>
-				<div className='project-title' onClick={() => handleClick('Project 2', Project2)} >Project 2</div>
-				<div className='project-title' onClick={() => handleClick('Project 3', Project3)} >Project 3</div>
-				<div className='project-title' onClick={() => handleClick('Project 4', Project4)} >Project 4</div>
-				<div className='project-title' onClick={() => handleClick('Project 5', Project5)} >Project 5</div>
+		<div id="projects-container">
+			<aside id="project-title-container">
+				<div className="project-title" onClick={() => handleClick('Project 1', Project1)}>
+					Project 1
+				</div>
+				<div className="project-title" onClick={() => handleClick('Project 2', Project2)}>
+					Project 2
+				</div>
+				<div className="project-title" onClick={() => handleClick('Project 3', Project3)}>
+					Project 3
+				</div>
+				<div className="project-title" onClick={() => handleClick('Project 4', Project4)}>
+					Project 4
+				</div>
+				<div className="project-title" onClick={() => handleClick('Project 5', Project5)}>
+					Project 5
+				</div>
 			</aside>
-			<section id='project-info'>
+			<section id="project-info">
 				<Project title={currentProject} projectImage={currentProjectImage} text={currentProjectText} />
 			</section>
 		</div>
-	)
+	);
 };
 export default Projects;
